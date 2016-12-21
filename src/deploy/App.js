c358563c760ecefe190b2d99a18afbb551ed94c7
David Rubert <david.rubert@gmail.com>
2016-12-21 07:35:17 +0100

    deploy step

---
+++ src/deploy/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import NavBar from './components/navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <NavBar />
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './deploy/App';
+import '../node_modules/@blueprintjs/core/dist/blueprint.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
