febda073a0e45c1699c58bf73f3a4198031ad8f1
David Rubert <david.rubert@gmail.com>
2017-01-23 19:33:31 +0100

    update

--- src/cli/collect/iberdrola.js
+++ src/cli/collect/iberdrola.js
@@ -66,6 +66,8 @@ const waitFor = async (data) => {
     height: 800
   })
 
+  page.setting('userAgent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36')
+
   page.property('onConsoleMessage', (msg, lineNum, sourceId) =>
     console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")')
   )
